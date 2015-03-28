dbf1549815d00275915838010f6ece0f0387a256
3DPrintScanner <1988anthonythomas@gmail.com>
2015-03-28 23:59:13 +0100

    added settings for eve interface

--- settings.py
+++ settings.py
@@ -1,1 +1,29 @@
-DOMAIN = {'people': {}}
+DOMAIN = {'trains': {}}
+
+schema = {
+	'platform': {
+		'type': 'string',
+		'minlength': 1,
+		'maxlength': 3,
+	},
+	'destination' : {
+		'type': 'string',
+		'minlength': 3,
+		'maxlength': 35,
+	},
+	'schedule' : {
+		'type': 'datetime'
+	},
+	'due': {
+		'type':'string'
+	},
+	'operator': {
+		'type' : 'string',
+		'minlength': 4,
+		'maxlength': 35
+	}
+
+	
+
+
+}
